d4cbc8b9f14c0f2ec7de41f22a5eaa13f8edcb45
Katarzyna Rycerz <kzajac@agh.edu.pl>
2017-06-02 10:13:52 +0200

    second commit

--- src/AGH_Julia_Lab1.jl
+++
@@ -1,8 +0,0 @@
-module AGH_Julia_Lab1
-
-# package code goes here
-function hello()
- "hello "
-end
-
-end # module
--- test/runtests.jl
+++
@@ -1,5 +0,0 @@
-using AGH_Julia_Lab1
-using Base.Test
-
-# write your own tests here
-@test 1 == 2
